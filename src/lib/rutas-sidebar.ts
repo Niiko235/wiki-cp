5e626ae56e413e22f7ea24726c45bc6e43caf885
Niiko235 <nicolaseplaza@gmail.com>
2026-08-23 01:10:38 -0500

    "rutes"

---
+++ src/lib/rutas-sidebar.ts
@@ -0,0 +1,21 @@
+type rutasType = {
+    nombreCategoria: string,
+    contenido: {
+        nombreContenido: string,
+        slugContenido: string;
+    }[]
+}[]
+
+
+export const rutas : rutasType = [
+{
+    nombreCategoria: "Estructuras",
+    contenido: [{
+        nombreContenido: "Segment Tree",
+        slugContenido: "segment-tree"
+    }]
+
+}
+]
+
+
