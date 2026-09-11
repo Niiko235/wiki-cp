0fc55f807dc0b36945fba2e2f139c2af768699e7
Niiko235 <nicolaseplaza@gmail.com>
2026-09-11 15:47:00 -0500

    "fuatures thanks page"

---
+++ src/lib/info-autores.ts
@@ -0,0 +1,52 @@
+export const colbaradores = [
+    'Jeank'
+]
+
+type Creadores = {
+    nombre: string,
+    descripcion: string,
+    perfilGit: string,
+    carrera: string,
+    semestre: string,
+    avatar?: string,
+}[]
+
+export const creadores : Creadores = [
+    {
+        nombre: 'Nicolas Plaza',
+        descripcion: 'Fundador del proyecto y tech lead. Lleva 3 años en el semillero y ha participado en el ICPC desde segundo semestre. Le apasiona la arquitectura de software y compartir lo que aprende.',
+        perfilGit: 'niiko235',
+        carrera: 'Ing. de Sistemas',
+        semestre: '8° semestre',
+        avatar: '../../public/avatars/nico.png'
+    },
+    {
+        nombre: 'Andres Lasso',
+        descripcion: '',
+        perfilGit: '',
+        carrera: 'Ing. de Sistemas',
+        semestre: '° semestre'
+    },
+    {
+        nombre: 'Hector Echeverry',
+        descripcion: '',
+        perfilGit: '',
+        carrera: 'Ing. de Sistemas',
+        semestre: '° semestre'
+    },
+    {
+        nombre: 'Juan Guzman',
+        descripcion: '',
+        perfilGit: '',
+        carrera: 'Ing. de Sistemas',
+        semestre: '° semestre'
+    },
+    {
+        nombre: 'Santiago Hernandez',
+        descripcion: '',
+        perfilGit: '',
+        carrera: 'Ing. de Sistemas',
+        semestre: '° semestre'
+    },
+]
+
